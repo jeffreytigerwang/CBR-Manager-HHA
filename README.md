74ecf44c0a1d6f8baa3066421afab35f634d90be
compscikai <kaigra@icloud.com>
2021-02-06 13:04:51 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instructions
+
+Clone Repo
+
+`cd tut-nodejs-backend-api`
+
+`npm install`
---
+++ server.js
@@ -0,0 +1,31 @@
+// Code modified from https://bezkoder.com/node-js-express-sequelize-mysql/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the CBR Application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+

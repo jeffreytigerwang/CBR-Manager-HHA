39a3709f5054de898413c473deeb2e14dcc81f67
Jeffrey Wang <jtw14@sfu.ca>
2021-02-07 23:10:34 -0500

    Add footer menu
    Add the initial bottom navigation menu. Currently includes only one item (List). Enabled switching between fragments through the menu.

--- app/src/main/java/com/example/cbr/activities/HomeActivity.java
+++ app/src/main/java/com/example/cbr/activities/HomeActivity.java
@@ -1,15 +1,21 @@
 package com.example.cbr.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.cbr.R;
 import com.example.cbr.fragments.TempHomeFragment;
 import com.example.cbr.fragments.base.BaseActivity;
 import com.example.cbr.fragments.clientlist.ClientListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends BaseActivity
         implements TempHomeFragment.TempHomeFragmentInterface
@@ -20,8 +26,26 @@ public class HomeActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
         swapToHomeFragment();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.fragment_clientlist:
+                            selectedFragment = new ClientListFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.homeFragmentContainer, selectedFragment).commit();
+                    return true;
+                }
+            };
 
     public void swapToHomeFragment() {
         TempHomeFragment tempHomeFragment = TempHomeFragment.newInstance();
@@ -32,6 +56,7 @@ public class HomeActivity extends BaseActivity
     public void swapToClientList() {
         ClientListFragment clientListFragment = ClientListFragment.newInstance();
         replaceFragment(R.id.homeFragmentContainer, clientListFragment, ClientListFragment.getFragmentTag());
+
     }
 
 

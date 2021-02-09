082437eef5c30297f5a0539ae6478b88462f9bec
JohnnyD <johnnyd@sfu.ca>
2021-02-09 00:34:37 -0800

    Create New Visit Activity

---
+++ app/src/main/java/com/example/cbr/activities/NewVisitActivity.java
@@ -0,0 +1,18 @@
+package com.example.cbr.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cbr.R;
+
+public class NewVisitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_visit);
+
+
+    }
+}

cfc18a7db64cafc5db166c5cd07686a0152cc859
Jeffrey Wang <jtw14@sfu.ca>
2021-02-15 16:46:50 -0500

    Implement clientlist page design
    Implement clientlist page design and clitentlist (list) item design.

--- app/src/main/java/com/example/cbr/fragments/clientlist/ClientListFragment.java
+++ app/src/main/java/com/example/cbr/fragments/clientlist/ClientListFragment.java
@@ -1,12 +1,19 @@
 package com.example.cbr.fragments.clientlist;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.example.cbr.R;
 import com.example.cbr.databinding.FragmentClientlistBinding;
 import com.example.cbr.fragments.base.BaseFragment;
 
@@ -15,17 +22,24 @@ public class ClientListFragment extends BaseFragment implements ClientListContra
     private FragmentClientlistBinding binding;
     private ClientListContract.Presenter clientListPresenter;
 
+
+
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setPresenter(new ClientListPresenter(this));
 
         // View binding so that findViewById() doesn't have to be used
         binding = FragmentClientlistBinding.inflate(inflater, container, false);
+        displayString("Patient List");
 
-        setupButton();
+        //setupButton();
 
-        return binding.getRoot();
+        View view = binding.getRoot();
+
+        return view;
     }
+
+
 
     @Override
     public void onDestroyView() {

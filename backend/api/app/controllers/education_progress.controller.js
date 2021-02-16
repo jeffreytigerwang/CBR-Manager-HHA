9a56ad0f30382a7d57b8e70e818d1364df706f0e
Jonny Lin <jonnyl@sfu.ca>
2021-02-15 17:21:00 -0800

    Add health, education and social controllers

---
+++ backend/api/app/controllers/education_progress.controller.js
@@ -0,0 +1,131 @@
+const db = require("../models");
+const EducationProgress = db.education_progress;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new data
+exports.create = (req, res) => {
+  // Validate Request
+  if (!req.body.help_provided) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create Item
+  const educationProgress = {
+    help_provided: req.body.help_provided,
+    goal_outcome: req.body.goal_outcome,
+    conclusion: req.body.goal_outcome
+  };
+
+  // Save item in database
+  EducationProgress.create(educationProgress)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error occured creating a educationProgress."
+      });
+    });
+
+};
+
+// Retrieve all data from the database.
+exports.findAll = (req, res) => {
+ 
+  EducationProgress.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving educationProgress."
+      });
+    });
+};
+
+// Find a single educationProgress with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  EducationProgress.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving educationProgress with id=" + id
+      });
+    });
+};
+
+// Update a data item by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  EducationProgress.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "educationProgress was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update educationProgress with id=${id}. Maybe item was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating educationProgress with id=" + id
+      });
+    });
+};
+
+// Delete a data item with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  EducationProgress.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "educationProgress was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete educationProgress with id=${id}. Maybe item was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not educationProgress with id=" + id
+      });
+    });
+};
+
+// Delete all data from the database.
+exports.deleteAll = (req, res) => {
+  EducationProgress.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} educationProgress were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all items."
+      });
+    });
+};
---
+++ backend/api/app/controllers/social_aspect.controller.js
@@ -0,0 +1,133 @@
+const db = require("../models");
+const SocialAspects = db.social_aspects;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new data
+exports.create = (req, res) => {
+  // Validate Request
+  if (!req.body.risk_level) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create Item
+  const socialAspect = {
+    risk_level: req.body.risk_level,
+    requirements: req.body.requirements,
+    goal: req.body.goal,
+    progress_ids: req.body.progress_ids
+
+  };
+
+  // Save item in database
+  SocialAspects.create(socialAspect)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error occured creating a socialAspect."
+      });
+    });
+
+};
+
+// Retrieve all data from the database.
+exports.findAll = (req, res) => {
+
+  SocialAspects.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving SocialAspects."
+      });
+    });
+};
+
+// Find a single socialAspect with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  SocialAspects.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving socialAspect with id=" + id
+      });
+    });
+};
+
+// Update a data item by the id in the request
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  SocialAspects.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "socialAspect was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update socialAspect with id=${id}. Maybe item was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating socialAspect with id=" + id
+      });
+    });
+};
+
+// Delete a data item with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  SocialAspects.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "socialAspect was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete socialAspect with id=${id}. Maybe item was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not socialAspect with id=" + id
+      });
+    });
+};
+
+// Delete all data from the database.
+exports.deleteAll = (req, res) => {
+  SocialAspects.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} SocialAspects were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all items."
+      });
+    });
+};

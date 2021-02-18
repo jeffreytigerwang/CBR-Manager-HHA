a1e8c08709875c367a9dd96207a682d11d1a71ac
compscikai <kaigra@icloud.com>
2021-02-17 18:22:04 -0800

    Fixes 44: Updated routes to camelcase from underscore

--- backend/api/app/routes/health_progress.routes.js
+++ backend/api/app/routes/health_progress.routes.js
@@ -1,26 +1,26 @@
 module.exports = app => {
     const health_progress = require("../controllers/health_progress.controller.js");
-  
+
     var router = require("express").Router();
-  
+
     // Create a new item
     router.post("/", health_progress.create);
-  
+
     // Retrieve all items
     router.get("/", health_progress.findAll);
-  
+
     // Retrieve a single item with id
     router.get("/:id", health_progress.findOne);
-  
+
     // Update a item with id
     router.put("/:id", health_progress.update);
 
     // Delete a item with id
     router.delete("/:id", health_progress.delete);
-  
+
     // Delete all items
     router.delete("/", health_progress.deleteAll);
-  
-    app.use('/api/health_progress', router);
+
+    app.use('/api/healthProgress', router);
   };
-  
+
--- backend/api/app/routes/social_aspect.routes.js
+++ backend/api/app/routes/social_aspect.routes.js
@@ -1,26 +1,26 @@
 module.exports = app => {
     const social_aspects = require("../controllers/social_aspect.controller.js");
-  
+
     var router = require("express").Router();
-  
+
     // Create a new item
     router.post("/", social_aspects.create);
-  
+
     // Retrieve all items
     router.get("/", social_aspects.findAll);
-  
+
     // Retrieve a single item with id
     router.get("/:id", social_aspects.findOne);
-  
+
     // Update a item with id
     router.put("/:id", social_aspects.update);
-  
+
     // Delete a item with id
     router.delete("/:id", social_aspects.delete);
-  
+
     // Delete all items
     router.delete("/", social_aspects.deleteAll);
-  
-    app.use('/api/social_aspects', router);
+
+    app.use('/api/socialAspects', router);
   };
-  
+

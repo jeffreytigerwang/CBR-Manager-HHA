7debc57bc2255d1f83c40a4ef095cbcf83b5d439
compscikai <kaigra@icloud.com>
2021-02-28 15:27:00 -0800

    Fixes 95: Implemented Unique users-email property & tests to check

--- backend/api/app/models/index.js
+++ backend/api/app/models/index.js
@@ -4,6 +4,7 @@ const dbConfig = require("../config/db.config.js");
 // sequalize is the instance
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  logging: false,
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,

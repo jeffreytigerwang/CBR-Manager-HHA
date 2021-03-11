d4e224d4eadadb6494db2d2286b51ac945d21151
luduo <luduol@sfu.ca>
2021-03-10 21:59:54 -0800

    "Change hardcoded gender to string.xml"

--- app/src/main/java/com/example/cbr/fragments/clientpage/ClientPageFragment.java
+++ app/src/main/java/com/example/cbr/fragments/clientpage/ClientPageFragment.java
@@ -70,7 +70,7 @@ public class ClientPageFragment extends BaseFragment implements ClientPageContra
     }
 
     private void setupClientInfoCard() {
-        binding.clientPageGenderText.setText(String.format("Gender: %s", clientInfo.getGender()));
+        binding.clientPageGenderText.setText(getString(R.string.gender_clientpage, clientInfo.getGender()));
         binding.clientPageNameText.setText(clientInfo.getFullName());
         binding.clientPageLocationText.setText(clientInfo.getZoneLocation());
         binding.clientPageAgeText.setText(getString(R.string.age_clientpage, clientInfo.getAge().toString()));

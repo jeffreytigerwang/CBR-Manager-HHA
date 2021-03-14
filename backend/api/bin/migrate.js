d994602fe2581a8b7021b8a8fe447ecce8a4d57f
compscikai <kaigra@icloud.com>
2021-03-14 15:30:00 -0700

    Fix 100: Added visits, health progress, and health acpect for Najwa Sarpong

--- backend/api/bin/migrate.js
+++ backend/api/bin/migrate.js
@@ -18,8 +18,27 @@ async function seedData() {
     else { return 0; }
   })
 
+  const healthAspectNotEmpty = await db.health_aspect.findByPk(1).then(data => {
+    console.log('health aspect returned: ' + data);
+    if (data) { return 1; }
+    else { return 0; }
+  })
+
+  const visitsNotEmpty = await db.visits.findByPk(1).then(data => {
+    console.log('visits returned: ' + data);
+    if (data) { return 1; }
+    else { return 0; }
+  })
+
+  const healthProgressNotEmpty = await db.health_progress.findByPk(1).then(data => {
+    console.log('health progress returned: ' + data);
+    if (data) { return 1; }
+    else { return 0; }
+  })
+
   var isEmpty = true;
-  if (usersNotEmpty || clientsNotEmpty) {
+  if (usersNotEmpty || clientsNotEmpty || healthAspectNotEmpty
+      || visitsNotEmpty || healthProgressNotEmpty) {
     isEmpty = false;
   }
 
@@ -88,6 +107,118 @@ async function seedData() {
             contactNumber: '111-111-1111',
             caregiverPresentForInterview: false,
     });
+    seed = await db.health_aspect.create({
+            rateHealth: 'medium risk',
+            describeHealth: 'Najwa has trouble walking',
+            setGoalForHealth: 'Najwa needs a cane or wheelchair',
+            clientId: 1234
+    });
+    seed = await db.visits.create({
+            isHealthChecked: true,
+            isEducationChecked: false,
+            isSocialChecked: false,
+            purposeOfVisit: 'checking on ability to walk',
+            dateOfVisit: '2021-02-10',
+            workerName: 'Amadi Yusuf',
+            visitGpsLocation: '49.99, -130.01',
+            visitZoneLocation: 'Bidibidi Zone 1',
+            villageNumber: 5,
+            clientId: 1234,
+            visitId: 9000
+    });
+    seed = await db.health_progress.create({
+            isWheelChairChecked: true,
+            isProstheticChecked: false,
+            isOrthoticChecked: false,
+            isWheelChairRepairChecked: false,
+            isReferralToHCChecked: true,
+            isHealthAdviceChecked: true,
+            isHealthAdvocacyChecked: false,
+            isHealthEncouragementChecked: false,
+            wheelChairDesc: "may need a wheelchair for weak legs",
+            prostheticDesc: "",
+            orthoticDesc: "",
+            wheelChairRepairDesc: "",
+            referralToHCDesc: "given direction and phone number",
+            healthAdviceDesc: "should get children to help with chores",
+            healthAdvocacyDesc: "",
+            healthEncouragementDesc: "",
+            healthOutcomeDesc: "",
+            healthGoalDesc: "",
+            clientId: 1234,
+            visitId: 9000
+     });
+     seed = await db.visits.create({
+            isHealthChecked: true,
+            isEducationChecked: false,
+            isSocialChecked: false,
+            purposeOfVisit: 'checking if she was able to get a wheelchair',
+            dateOfVisit: '2021-02-20',
+            workerName: 'Amadi Yusuf',
+            visitGpsLocation: '49.99, -130.01',
+            visitZoneLocation: 'Bidibidi Zone 1',
+            villageNumber: 5,
+            clientId: 1234,
+            visitId: 9001
+    });
+    seed = await db.health_progress.create({
+            isWheelChairChecked: true,
+            isProstheticChecked: false,
+            isOrthoticChecked: false,
+            isWheelChairRepairChecked: false,
+            isReferralToHCChecked: false,
+            isHealthAdviceChecked: true,
+            isHealthAdvocacyChecked: false,
+            isHealthEncouragementChecked: false,
+            wheelChairDesc: "her kid went to health center and got her a wheelchair",
+            prostheticDesc: "",
+            orthoticDesc: "",
+            wheelChairRepairDesc: "",
+            referralToHCDesc: "",
+            healthAdviceDesc: "",
+            healthAdvocacyDesc: "",
+            healthEncouragementDesc: "",
+            healthOutcomeDesc: "",
+            healthGoalDesc: "",
+            clientId: 1234,
+            visitId: 9000
+    });
+    seed = await db.visits.create({
+            isHealthChecked: true,
+            isEducationChecked: false,
+            isSocialChecked: false,
+            purposeOfVisit: 'heard her wheelchair broke',
+            dateOfVisit: '2021-02-25',
+            workerName: 'Amadi Yusuf',
+            visitGpsLocation: '49.99, -130.01',
+            visitZoneLocation: 'Bidibidi Zone 1',
+            villageNumber: 5,
+            clientId: 1234,
+            visitId: 9002
+    });
+    seed = await db.health_progress.create({
+            isWheelChairChecked: false,
+            isProstheticChecked: false,
+            isOrthoticChecked: false,
+            isWheelChairRepairChecked: true,
+            isReferralToHCChecked: false,
+            isHealthAdviceChecked: false,
+            isHealthAdvocacyChecked: false,
+            isHealthEncouragementChecked: false,
+            wheelChairDesc: "",
+            prostheticDesc: "",
+            orthoticDesc: "",
+            wheelChairRepairDesc: "Left wheel is broken. Set up appointment with health center",
+            referralToHCDesc: "",
+            healthAdviceDesc: "",
+            healthAdvocacyDesc: "",
+            healthEncouragementDesc: "",
+            healthOutcomeDesc: "",
+            healthGoalDesc: "Make sure she fixes her wheel chair",
+            clientId: 1234,
+            visitId: 9000
+    });
+    console.log('All seed data implemented');
   }
   await db.sequelize.close();
 }

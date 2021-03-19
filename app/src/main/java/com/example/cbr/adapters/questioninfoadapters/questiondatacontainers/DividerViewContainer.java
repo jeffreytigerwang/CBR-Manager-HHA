039202d48f8e6a0cca00f2f4c250818ce213c1f1
compscikai <kaigra@icloud.com>
2021-03-18 20:15:19 -0700

    Fix 96: Remove file tracking on moved android files

--- app/src/main/java/com/example/cbr/adapters/questioninfoadapters/questiondatacontainers/DividerViewContainer.java
+++
@@ -1,7 +0,0 @@
-package com.example.cbr.adapters.questioninfoadapters.questiondatacontainers;
-
-public class DividerViewContainer extends QuestionDataContainer {
-    public DividerViewContainer() {
-        super(DIVIDER_VIEW_TYPE);
-    }
-}
--- app/src/main/java/com/example/cbr/fragments/base/BasePresenter.java
+++
@@ -1,4 +0,0 @@
-package com.example.cbr.fragments.base;
-
-public interface BasePresenter {
-}

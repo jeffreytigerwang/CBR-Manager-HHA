f8fe2c6a04fcbca9119b4d32d0a3d474fe136862
compscikai <kaigra@icloud.com>
2021-03-22 21:56:23 -0700

    Fix 116: Implement Referral database model

---
+++ backend/api/app/routes/referral.routes.js
@@ -0,0 +1,26 @@
+module.exports = app => {
+    const referral = require("../controllers/referral.controller.js");
+
+    var router = require("express").Router();
+
+    // Create a new item
+    router.post("/", referrals.create);
+
+    // Retrieve all items
+    router.get("/", referrals.findAll);
+
+    // Retrieve a single item with id
+    router.get("/:id", referrals.findOne);
+
+    // Update a item with id
+    router.put("/:id", referrals.update);
+
+    // Delete a item with id
+    router.delete("/:id", referrals.delete);
+
+    // Delete all items
+    router.delete("/", referrals.deleteAll);
+
+    app.use('/api/referrals', router);
+  };
+

4251dd451ddb2ad4ca95cbd3240c16e93da5cd0f
Jeffrey Wang <jtw14@sfu.ca>
2021-04-02 17:58:15 -0400

    Location permission
    Check location permission in map fragment. If user grant permission -> initialize the map.

--- app/src/main/java/com/example/cbr/fragments/map/MapFragment.java
+++ app/src/main/java/com/example/cbr/fragments/map/MapFragment.java
@@ -1,15 +1,23 @@
 package com.example.cbr.fragments.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.cbr.R;
+import com.example.cbr.activities.HomeActivity;
 import com.example.cbr.databinding.FragmentClientlistBinding;
 import com.example.cbr.databinding.FragmentMapBinding;
 import com.example.cbr.fragments.base.BaseFragment;
@@ -23,9 +31,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends BaseFragment implements MapContract.View {
+    private static  final String TAG = "MapFragment";
     private  MapContract.Presenter mapContractPresenter;
     private FragmentMapBinding binding;
     private GoogleMap mMap;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private Boolean mLocationPermissionsGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
     @Nullable
     @Override
@@ -34,6 +47,36 @@ public class MapFragment extends BaseFragment implements MapContract.View {
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
 
+//        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+//                .findFragmentById(R.id.map);
+//
+//        mapFragment.getMapAsync(new OnMapReadyCallback() {
+//            @Override
+//            public void onMapReady(GoogleMap googleMap) {
+//                mMap = googleMap;
+//                // Add a marker in Uganda and move the camera
+//                LatLng uganda = new LatLng(1.3733, 32.2903);
+//                mMap.addMarker(new MarkerOptions().position(uganda).title(getString(R.string.marker_in_Uganda)));
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(uganda));
+//            }
+//        });
+
+        getLocationPermission();
+
+        View view = binding.getRoot();
+        return view;
+    }
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    public static String getFragmentTag() {
+        return MapFragment.class.getSimpleName();
+    }
+
+    private void initMap(){
+        Log.d(TAG, "Map is ready.");
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
 
@@ -47,17 +90,51 @@ public class MapFragment extends BaseFragment implements MapContract.View {
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(uganda));
             }
         });
-
-        View view = binding.getRoot();
-        return view;
     }
 
-    public static MapFragment newInstance() {
-        return new MapFragment();
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions.");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(getContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+                initMap();
+            }
+            else{
+                requestPermissions(permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+        else{
+            requestPermissions(permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
     }
 
-    public static String getFragmentTag() {
-        return MapFragment.class.getSimpleName();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionResult: called.");
+        mLocationPermissionsGranted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0){
+                    for (int i = 0; i < grantResults.length; i++){
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionResult: permission failed.");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionResult: permission granted.");
+                    mLocationPermissionsGranted = true;
+                    initMap();
+                }
+        }
     }
 
     @Override

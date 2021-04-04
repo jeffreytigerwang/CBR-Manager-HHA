a5a2961934cb42ef6f064bb14d70586e74723e0b
Jeffrey Wang <jtw14@sfu.ca>
2021-04-04 02:33:40 -0400

    Implement search and display device
    Implement a search bar for the map. Display user device's location (blue dot).

--- app/src/main/java/com/example/cbr/fragments/map/MapFragment.java
+++ app/src/main/java/com/example/cbr/fragments/map/MapFragment.java
@@ -2,64 +2,65 @@ package com.example.cbr.fragments.map;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.nfc.Tag;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
 
 import com.example.cbr.R;
-import com.example.cbr.activities.HomeActivity;
-import com.example.cbr.databinding.FragmentClientlistBinding;
 import com.example.cbr.databinding.FragmentMapBinding;
 import com.example.cbr.fragments.base.BaseFragment;
-import com.example.cbr.fragments.clientlist.ClientListContract;
-import com.example.cbr.fragments.clientlist.ClientListPresenter;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends BaseFragment implements MapContract.View {
-    private static  final String TAG = "MapFragment";
-    private  MapContract.Presenter mapContractPresenter;
+    private static final String TAG = "MapFragment";
+    private MapContract.Presenter mapContractPresenter;
     private FragmentMapBinding binding;
     private GoogleMap mMap;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private Boolean mLocationPermissionsGranted = false;
+    private Boolean locationPermissionsGranted = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private EditText searchText;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setPresenter(new MapPresenter(this));
-
         binding = FragmentMapBinding.inflate(inflater, container, false);
 
-//        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-//                .findFragmentById(R.id.map);
-//
-//        mapFragment.getMapAsync(new OnMapReadyCallback() {
-//            @Override
-//            public void onMapReady(GoogleMap googleMap) {
-//                mMap = googleMap;
-//                // Add a marker in Uganda and move the camera
-//                LatLng uganda = new LatLng(1.3733, 32.2903);
-//                mMap.addMarker(new MarkerOptions().position(uganda).title(getString(R.string.marker_in_Uganda)));
-//                mMap.moveCamera(CameraUpdateFactory.newLatLng(uganda));
-//            }
-//        });
 
         getLocationPermission();
 
@@ -67,15 +68,8 @@ public class MapFragment extends BaseFragment implements MapContract.View {
         return view;
     }
 
-    public static MapFragment newInstance() {
-        return new MapFragment();
-    }
 
-    public static String getFragmentTag() {
-        return MapFragment.class.getSimpleName();
-    }
-
-    private void initMap(){
+    private void initMap() {
         Log.d(TAG, "Map is ready.");
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -83,14 +77,66 @@ public class MapFragment extends BaseFragment implements MapContract.View {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
+                //Toast.makeText(getContext(), "test123", Toast.LENGTH_SHORT).show();
                 mMap = googleMap;
-                // Add a marker in Uganda and move the camera
-                LatLng uganda = new LatLng(1.3733, 32.2903);
-                mMap.addMarker(new MarkerOptions().position(uganda).title(getString(R.string.marker_in_Uganda)));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(uganda));
+
+                if (locationPermissionsGranted){
+                    getDeviceLocation();
+
+                    // Add a marker in Uganda and move the camera
+                    LatLng uganda = new LatLng(1.3733, 32.2903);
+                    mMap.addMarker(new MarkerOptions().position(uganda).title(getString(R.string.marker_in_Uganda)));
+
+                    if (ActivityCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    mMap.setMyLocationEnabled(true);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+                    init();
+                }
             }
         });
     }
+
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        try{
+            if(locationPermissionsGranted){
+                Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+                        }
+                        else{
+                            Log.d(TAG, "conComplete: current location is null.");
+                            Toast.makeText(getContext(), "Unable to get current location",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+        catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
 
     private void getLocationPermission(){
         Log.d(TAG, "getLocationPermission: getting location permissions.");
@@ -101,7 +147,7 @@ public class MapFragment extends BaseFragment implements MapContract.View {
                 FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             if (ContextCompat.checkSelfPermission(getContext(),
                     COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                mLocationPermissionsGranted = true;
+                locationPermissionsGranted = true;
                 initMap();
             }
             else{
@@ -115,23 +161,73 @@ public class MapFragment extends BaseFragment implements MapContract.View {
         }
     }
 
+    private void init(){
+        Log.d(TAG, "init: initializing");
+        searchText = binding.inputSearch;
+
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE || keyEvent.getAction() == KeyEvent.ACTION_DOWN || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(getContext().INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(searchText.getWindowToken(), 0);
+                    geoLocate();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void geoLocate(){
+        Log.d(TAG, "geoLocate: geolocating");
+
+        String searchString = searchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(getContext());
+        List<Address> list = new ArrayList<>();
+        try{
+            // Only looks for 1 result.
+            list = geocoder.getFromLocationName(searchString, 1);
+        }
+        catch (IOException e){
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
+        }
+
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public static MapFragment newInstance() {
+        return new MapFragment();
+    }
+
+    public static String getFragmentTag() {
+        return MapFragment.class.getSimpleName();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.d(TAG, "onRequestPermissionResult: called.");
-        mLocationPermissionsGranted = false;
+        locationPermissionsGranted = false;
 
         switch (requestCode){
             case LOCATION_PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0){
                     for (int i = 0; i < grantResults.length; i++){
                         if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
-                            mLocationPermissionsGranted = false;
+                            locationPermissionsGranted = false;
                             Log.d(TAG, "onRequestPermissionResult: permission failed.");
                             return;
                         }
                     }
                     Log.d(TAG, "onRequestPermissionResult: permission granted.");
-                    mLocationPermissionsGranted = true;
+                    locationPermissionsGranted = true;
                     initMap();
                 }
         }

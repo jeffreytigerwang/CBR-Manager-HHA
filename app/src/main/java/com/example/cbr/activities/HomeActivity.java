2fc9ab4bab0cb8840562ea99d58242819f26a2c2
JohnnyD <johnnyd@sfu.ca>
2021-04-07 03:09:28 -0700

    Fix merge issue

--- app/src/main/java/com/example/cbr/activities/HomeActivity.java
+++ app/src/main/java/com/example/cbr/activities/HomeActivity.java
@@ -77,9 +77,6 @@ public class HomeActivity extends BaseActivity implements
                     case HomeFragmentPagerAdapter.DISCUSSION_POSITION:
                         bottomNavigationView.getMenu().findItem(R.id.bottomMenuDiscussion).setChecked(true);
                         break;
-                    case HomeFragmentPagerAdapter.NOTIFICATION_POSITION:
-                        bottomNavigationView.getMenu().findItem(R.id.bottomMenuNotification).setChecked(true);
-                        break;
                 }
 
                 viewPager.setUserInputEnabled(position != HomeFragmentPagerAdapter.MAP_POSITION);
@@ -108,10 +105,6 @@ public class HomeActivity extends BaseActivity implements
 
                             case R.id.bottomMenuDiscussion:
                                 currentTabPosition = HomeFragmentPagerAdapter.DISCUSSION_POSITION;
-                                break;
-
-                            case R.id.bottomMenuNotification:
-                                currentTabPosition = HomeFragmentPagerAdapter.NOTIFICATION_POSITION;
                                 break;
                         }
 

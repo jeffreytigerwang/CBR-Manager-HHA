165648b0f5812b5f31a552c99aa3a8753347c357
JohnnyD <johnnyd@sfu.ca>
2021-04-08 01:42:18 -0700

    Add JavaDoc to base methods

--- app/src/main/java/com/example/cbr/fragments/base/BaseFragment.java
+++ app/src/main/java/com/example/cbr/fragments/base/BaseFragment.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -47,6 +48,13 @@ public abstract class BaseFragment extends Fragment {
                 .show();
     }
 
+    /**
+     * Launch an alert dialog to request for a single permission.
+     *
+     * @param permission which {@link android.Manifest.permission permission} string to ask the user
+     * @param callback the callback to be called on the main thread when activity result is available
+     * @see Fragment#registerForActivityResult(ActivityResultContract, ActivityResultCallback)
+     * */
     protected void launchOneRequestPermission(
             @NonNull String permission,
             @NonNull ActivityResultCallback<Boolean> callback) {
@@ -57,6 +65,13 @@ public abstract class BaseFragment extends Fragment {
         requestPermissionLauncher.launch(permission);
     }
 
+    /**
+     * Launch multiple permission request in a single alert dialog.
+     *
+     * @param permissions series of {@link android.Manifest.permission permission} strings to ask the user
+     * @param callback the callback to be called on the main thread when activity result is available
+     * @see Fragment#registerForActivityResult(ActivityResultContract, ActivityResultCallback)
+     * */
     protected void launchRequestPermissions(
             @NonNull String[] permissions,
             @NonNull ActivityResultCallback<Map<String, Boolean>> callback) {
@@ -69,6 +84,11 @@ public abstract class BaseFragment extends Fragment {
         requestPermissionLauncher.launch(permissions);
     }
 
+    /**
+     * Check to see if the user has granted permission.
+     *
+     * @param permission what {@link android.Manifest.permission permission} to check for
+     * */
     protected boolean isPermissionGranted(@NonNull Context context, @NonNull String permission) {
         return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED;
     }

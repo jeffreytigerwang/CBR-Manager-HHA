b8550eb7bc20ed8e944db2ae270bc8b95b504216
David <dylu@sfu.ca>
2021-04-10 17:18:55 -0700

    Merged with most recent master and fixed errors.

--- app/src/main/java/com/example/cbr/models/ClientInfo.java
+++ app/src/main/java/com/example/cbr/models/ClientInfo.java
@@ -10,7 +10,6 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ClientInfo implements Serializable, Comparable<ClientInfo>{
-    private double nullCoordinateHandler = 300;
     private static final double nullCoordinateHandler = 300;
     private Integer clientId;
     private double latitude;

e7c15fe23a05ee2fa4998df3392aa025597345f1
Jonny Lin <jonnyl@sfu.ca>
2021-04-11 17:48:39 -0700

    Fixes #82: Setup methods in HomePagePresenter to get data

--- app/src/main/java/com/example/cbr/fragments/home/HomePageContract.java
+++ app/src/main/java/com/example/cbr/fragments/home/HomePageContract.java
@@ -2,9 +2,35 @@ package com.example.cbr.fragments.home;
 
 import com.example.cbr.fragments.base.BasePresenter;
 import com.example.cbr.fragments.base.BaseView;
+import com.example.cbr.models.ClientDisability;
+import com.example.cbr.models.ClientEducationAspect;
+import com.example.cbr.models.ClientHealthAspect;
+import com.example.cbr.models.ClientInfo;
+import com.example.cbr.models.ClientSocialAspect;
+import com.example.cbr.models.ReferralInfo;
+import com.example.cbr.models.VisitEducationQuestionSetData;
+import com.example.cbr.models.VisitGeneralQuestionSetData;
+import com.example.cbr.models.VisitHealthQuestionSetData;
+import com.example.cbr.models.VisitSocialQuestionSetData;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface HomePageContract {
     interface Presenter extends BasePresenter {
+        List<ClientInfo> getAllClients() throws IOException;
+        List<ClientDisability> getAllClientDisabilities() throws IOException;
+        List<ClientEducationAspect> getAllClientEducationAspect() throws IOException;
+        List<ClientHealthAspect> getAllClientHealthAspect() throws IOException;
+        List<ClientSocialAspect> getAllClientSocialAspect() throws IOException;
+
+        List<ReferralInfo> getAllReferrals() throws IOException;
+        List<VisitEducationQuestionSetData> getAllVisitEducationQuestions() throws IOException;
+        List<VisitGeneralQuestionSetData> getAllVisitGeneralQuestions() throws IOException;
+        List<VisitHealthQuestionSetData> getAllVisitHealthQuestions() throws IOException;
+        List<VisitSocialQuestionSetData> getAllVisitSocialQuestions() throws IOException;
+
+
     }
 
     interface View extends BaseView<Presenter>{
--- app/src/main/java/com/example/cbr/fragments/home/HomePagePresenter.java
+++ app/src/main/java/com/example/cbr/fragments/home/HomePagePresenter.java
@@ -1,10 +1,105 @@
 package com.example.cbr.fragments.home;
 
 
+import com.example.cbr.models.ClientDisability;
+import com.example.cbr.models.ClientEducationAspect;
+import com.example.cbr.models.ClientHealthAspect;
+import com.example.cbr.models.ClientInfo;
+import com.example.cbr.models.ClientSocialAspect;
+import com.example.cbr.models.ReferralInfo;
+import com.example.cbr.models.VisitEducationQuestionSetData;
+import com.example.cbr.models.VisitGeneralQuestionSetData;
+import com.example.cbr.models.VisitHealthQuestionSetData;
+import com.example.cbr.models.VisitSocialQuestionSetData;
+import com.example.cbr.retrofit.JsonPlaceHolderApi;
+import com.example.cbr.retrofit.RetrofitInit;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class HomePagePresenter implements HomePageContract.Presenter {
     private HomePageContract.View homePageView;
+    private Retrofit retrofit;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     public HomePagePresenter(HomePageContract.View homePageView) {
+        retrofit = RetrofitInit.getInstance();
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
         this.homePageView = homePageView;
+    }
+
+    @Override
+    public List<ClientInfo> getAllClients() throws IOException {
+        Call<List<ClientInfo>> call = jsonPlaceHolderApi.getClientsInfo();
+        Response<List<ClientInfo>> response = call.execute();
+        return response.body();
+    }
+
+    @Override
+    public List<ClientDisability> getAllClientDisabilities() throws IOException {
+        Call<List<ClientDisability>> call = jsonPlaceHolderApi.getClientDisability();
+        Response<List<ClientDisability>> response = call.execute();
+        return response.body();
+    }
+
+    @Override
+    public List<ClientEducationAspect> getAllClientEducationAspect() throws IOException {
+        Call<List<ClientEducationAspect>> call = jsonPlaceHolderApi.getClientEducationAspect();
+        Response<List<ClientEducationAspect>> response = call.execute();
+        return response.body();
+    }
+
+    @Override
+    public List<ClientHealthAspect> getAllClientHealthAspect() throws IOException {
+        Call<List<ClientHealthAspect>> call = jsonPlaceHolderApi.getClientHealthAspect();
+        Response<List<ClientHealthAspect>> response = call.execute();
+        return response.body();
+    }
+
+    @Override
+    public List<ClientSocialAspect> getAllClientSocialAspect() throws IOException {
+        Call<List<ClientSocialAspect>> call = jsonPlaceHolderApi.getClientSocialAspect();
+        Response<List<ClientSocialAspect>> response = call.execute();
+        return response.body();
+    }
+
+    @Override
+    public List<ReferralInfo> getAllReferrals() throws IOException {
+        Call<List<ReferralInfo>> call = jsonPlaceHolderApi.getReferralInfo();
+        Response<List<ReferralInfo>> response = call.execute();
+        return response.body();
+    }
+
+    @Override
+    public List<VisitEducationQuestionSetData> getAllVisitEducationQuestions() throws IOException {
+//        Call<List<VisitEducationQuestionSetData>> call = jsonPlaceHolderApi.();
+//        Response<List<VisitEducationQuestionSetData>> response = call.execute();
+        return null;
+    }
+
+    @Override
+    public List<VisitGeneralQuestionSetData> getAllVisitGeneralQuestions() throws IOException {
+//        Call<List<VisitGeneralQuestionSetData>> call = jsonPlaceHolderApi.getClientsInfo();
+//        Response<List<VisitGeneralQuestionSetData>> response = call.execute();
+        return null;
+    }
+
+    @Override
+    public List<VisitHealthQuestionSetData> getAllVisitHealthQuestions() throws IOException {
+//        Call<List<VisitHealthQuestionSetData>> call = jsonPlaceHolderApi.getClientsInfo();
+//        Response<List<VisitHealthQuestionSetData>> response = call.execute();
+        return null;
+    }
+
+    @Override
+    public List<VisitSocialQuestionSetData> getAllVisitSocialQuestions() throws IOException {
+//        Call<List<VisitSocialQuestionSetData>> call = jsonPlaceHolderApi.getClientsInfo();
+//        Response<List<VisitSocialQuestionSetData>> response = call.execute();
+
+        return null;
     }
 }
